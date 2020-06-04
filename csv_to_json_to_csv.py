ba4d9c0e68a525743801d8d9102c82fd1b6c17b6
sachitdtu <sachit.sharma@flipkart.com>
2020-06-04 13:06:38 +0530

    name change

--- csv_to_json_to_csv.py
+++ csv_to_json_to_csv.py
@@ -7,8 +7,8 @@ def explode(record):
     record = record.replace("\\", "").replace("\"{", "{").replace("}\"", "}")
 
     json_parsed = json.loads(record)
-    for r in json_parsed['placements']:
-        temp_res = [r['type'], r['percentage'], r['absoluteCost']]
+    for r in json_parsed['Key1']:
+        temp_res = [r['Kye11'], r['Kye12'], r['Kye13']]
         result.append(temp_res)
     return result
 
@@ -23,10 +23,10 @@ def write_to_file(file_path, rows):
 if __name__ == '__main__':
     results = []
     col = []
-    with open("/Users/sachit.sharma/Downloads/query_result.csv", "r") as infile:
+    with open("csv_file.csv", "r") as infile:
         col = infile.readline().strip().split(",")
         content = [i.strip().split(",", 4) for i in infile.readlines()]
-    col = list(map(lambda x:x.replace("\"",""), col))
+    col = list(map(lambda x: x.replace("\"", ""), col))
     results.append(col)
 
     df = pd.DataFrame(content, columns=col)
@@ -43,4 +43,4 @@ if __name__ == '__main__':
             exploded_rows = list(map(lambda x: csv_row[:3] + x, rows))
             results = results + exploded_rows
 
-    write_to_file("/Users/sachit.sharma/Downloads/res_query_result.csv", results)
+    write_to_file("res_csv_file.csv.csv", results)
